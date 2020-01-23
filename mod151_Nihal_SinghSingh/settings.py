959d31751a228106dec8e7368dca48d8db4f3bd8
nsinghsingh <nihal.singh@csbe.ch>
2020-01-23 09:39:43 +0100

    changed server to postgresql

--- mod151_Nihal_SinghSingh/settings.py
+++ mod151_Nihal_SinghSingh/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'mod151_Nihal_SinghSingh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'sml12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
