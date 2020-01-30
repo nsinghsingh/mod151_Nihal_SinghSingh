b07d2e3f30cd75e16436bf439ebe8a2f1eb6072b
nsinghsingh <nihal.singh@csbe.ch>
2020-01-30 10:34:17 +0100

    improved security

--- login/models.py
+++ login/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class User(models.Model):
     username = models.CharField(max_length=20)
-    password = models.CharField(max_length=45)
+    password = models.CharField(max_length=255)
